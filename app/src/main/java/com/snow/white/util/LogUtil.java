18207f4c2ef4df3fdf4a50e019ca4b124dce219e
xiejimin <xiejimin@021.com>
2020-04-23 14:42:52 +0800

    "2020-04-22 优化LoggerUtils.d()"

--- app/src/main/java/com/snow/white/util/LogUtil.java
+++ app/src/main/java/com/snow/white/util/LogUtil.java
@@ -1,10 +1,14 @@
 package com.snow.white.util;
 
+import android.util.Log;
+
 public class LogUtil {
 
     public static final boolean isDebug = true;
 
-    public void d() {
-
+    public void d(String tag, String message) {
+        if(isDebug){
+            Log.d(tag, message);
+        }
     }
 }
